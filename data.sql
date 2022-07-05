9d3d981d9cbc97cdc0bd2d6bee8d15e8b726f7f3
Pazzo97 <pazzomuk@gmail.com>
2022-07-05 12:34:54 +0200

    edited boolean

--- data.sql
+++ data.sql
@@ -1,6 +1,6 @@
 /* Populate database with sample data. */
 
-INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kgs) VALUES (1, 'Agumon', '2020-02-03', 0, '1', 10.23);
-INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kgs) VALUES (2, 'Gabumon', '2018-11-15', 2, '1', 8);
-INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kgs) VALUES (3, 'Pikachu', '2021-01-07', 1, '0', 15.04);
-INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kgs) VALUES (4, 'Devimon', '2017-05-12', 5, '1', 11);
+INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kgs) VALUES (1, 'Agumon', '2020-02-03', 0, 'true', 10.23);
+INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kgs) VALUES (2, 'Gabumon', '2018-11-15', 2, 'true', 8);
+INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kgs) VALUES (3, 'Pikachu', '2021-01-07', 1, 'false', 15.04);
+INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kgs) VALUES (4, 'Devimon', '2017-05-12', 5, 'true', 11);
--- queries.sql
+++ queries.sql
@@ -1,10 +1,10 @@
 /*Queries that provide answers to the questions from all projects.*/
 
 SELECT * FROM animals WHERE name LIKE '%mon';
-SELECT name FROM animals WHERE date_of_birth BETWEEN '2016-12-31' AND '2019-01-01';
-SELECT name FROM animals WHERE neutered = '1' AND escape_attempts < 3;
+SELECT name FROM animals WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-12-31';
+SELECT name FROM animals WHERE neutered = 'true' AND escape_attempts < 3;
 SELECT date_of_birth FROM animals WHERE name = 'Agumon' OR name = 'Pikachu';
 SELECT name, escape_attempts FROM animals WHERE weight_kgs > 10.5;
-SELECT * FROM animals WHERE neutered = '1';
+SELECT * FROM animals WHERE neutered = 'true';
 SELECT * FROM animals WHERE name <> 'Gabumon';
 SELECT * FROM animals WHERE weight_kgs >= '10.4' AND weight_kgs <= '17.3';
